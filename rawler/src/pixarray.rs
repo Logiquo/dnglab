50aca5b115f0db5b8ef7d78c41c179e1c13337b4
Logiquo <fanyongda2012@gmail.com>
2026-09-06 08:55:32 -0400

    add BorderPadding trait

--- rawler/src/pixarray.rs
+++ rawler/src/pixarray.rs
@@ -591,6 +591,108 @@ where
 
 unsafe impl<T, const N: usize> Sync for Color2DPtr<T, N> {}
 
+/// Border-coordinate access for 2D pixel containers such as [`crate::pixarray::Pix2D`]
+/// and [`crate::pixarray::Color2D`].
+///
+/// Negative or out-of-range coordinates are mapped back into the image according
+/// to the selected edge-padding rule. For an input row `a b c d e f g` and a
+/// two-pixel border, the rules are:
+pub trait BorderPadding<T> {
+  /// Access a pixel using reflected padding.
+  ///
+  /// When `repeat_edge` is `true`, the edge pixel is repeated: `b a | a b c d
+  /// e f g | g f`. This is OpenCV `BORDER_REFLECT`, NumPy
+  /// `pad(mode="symmetric")`, and SciPy `ndimage` `mode="reflect"`.
+  ///
+  /// When `repeat_edge` is `false`, the edge pixel is not repeated: `c b | a b
+  /// c d e f g | f e`. This is OpenCV `BORDER_REFLECT_101` (also
+  /// `BORDER_DEFAULT`), NumPy `pad(mode="reflect")`, and SciPy `ndimage`
+  /// `mode="mirror"`.
+  fn at_reflect(&self, row: isize, col: isize, repeat_edge: bool) -> &T;
+
+  /// Access a pixel using constant-value padding.
+  ///
+  /// Out-of-range coordinates return `constant`, e.g. passing `0` produces
+  /// `0 0 | a b c d e f g | 0 0`. This is OpenCV `BORDER_CONSTANT`, NumPy
+  /// `pad(mode="constant", constant_values=constant)`, and SciPy `ndimage`
+  /// `mode="constant", cval=constant`.
+  fn at_padding<'a>(&'a self, row: isize, col: isize, constant: &'a T) -> &'a T;
+}
+
+impl<T> BorderPadding<T> for Pix2D<T>
+where
+  T: Copy + Default + Send,
+{
+  #[inline(always)]
+  fn at_reflect(&self, row: isize, col: isize, repeat_edge: bool) -> &T {
+    if repeat_edge {
+      self.at(reflect_repeat_edge(row, self.height), reflect_repeat_edge(col, self.width))
+    } else {
+      self.at(reflect_unique_edge(row, self.height), reflect_unique_edge(col, self.width))
+    }
+  }
+
+  #[inline(always)]
+  fn at_padding<'a>(&'a self, row: isize, col: isize, constant: &'a T) -> &'a T {
+    if row >= 0 && (row as usize) < self.height && col >= 0 && (col as usize) < self.width {
+      self.at(row as usize, col as usize)
+    } else {
+      constant
+    }
+  }
+}
+
+impl<T, const N: usize> BorderPadding<[T; N]> for Color2D<T, N>
+where
+  T: Copy + Clone + Default + Send,
+  [T; N]: Default,
+{
+  #[inline(always)]
+  fn at_reflect(&self, row: isize, col: isize, repeat_edge: bool) -> &[T; N] {
+    if repeat_edge {
+      self.at(reflect_repeat_edge(row, self.height), reflect_repeat_edge(col, self.width))
+    } else {
+      self.at(reflect_unique_edge(row, self.height), reflect_unique_edge(col, self.width))
+    }
+  }
+
+  #[inline(always)]
+  fn at_padding<'a>(&'a self, row: isize, col: isize, constant: &'a [T; N]) -> &'a [T; N] {
+    if row >= 0 && (row as usize) < self.height && col >= 0 && (col as usize) < self.width {
+      self.at(row as usize, col as usize)
+    } else {
+      constant
+    }
+  }
+}
+
+#[inline(always)]
+fn reflect_repeat_edge(i: isize, n: usize) -> usize {
+  if 0 <= i && i < n as isize {
+    return i as usize;
+  }
+  let n = n as isize;
+  debug_assert!(n > 0, "cannot reflect an empty dimension");
+  let period = n * 2;
+  let index = i.rem_euclid(period);
+  (if index < n { index } else { period - 1 - index }) as usize
+}
+
+#[inline(always)]
+fn reflect_unique_edge(i: isize, n: usize) -> usize {
+  if 0 <= i && i < n as isize {
+    return i as usize;
+  }
+  let n = n as isize;
+  debug_assert!(n > 0, "cannot reflect an empty dimension");
+  if n == 1 {
+    return 0;
+  }
+  let period = (n - 1) * 2;
+  let index = i.rem_euclid(period);
+  (if index < n { index } else { period - index }) as usize
+}
+
 #[macro_export]
 macro_rules! alloc_image_f32_plain {
   ($width:expr, $height:expr, $dummy: expr) => {{
@@ -601,10 +703,6 @@ macro_rules! alloc_image_f32_plain {
       $crate::pixarray::PixF32::new_uninit($width, $height)
     } else {
       $crate::pixarray::PixF32::new($width, $height)
-    }
-  }};
-}
-
 #[macro_export]
 macro_rules! alloc_image_plain {
   ($width:expr, $height:expr, $dummy: expr) => {{
